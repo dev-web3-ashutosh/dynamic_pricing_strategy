f3e28a3566989aecc2b5b082fd73a06f8ee2da60
Ashutosh Tripathi <dev.web3.ashutosh@gmail.com>
2024-09-01 15:24:59 +0530

    Update dynamic_pricing.py

--- dynamic_pricing.py
+++ dynamic_pricing.py
@@ -2,3 +2,11 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 
+data=pd.read_csv('dynamic_pricing.csv')
+# print(data.head())
+
+# describe data
+# print(data.describe())
+
+fig=px.scatter(data, x='Expected_Ride_Duration', y='Historical_Cost_of_Ride', title='Expected Ride Duration vs. Historical Cost', trendline='ols', trendline_color_override='red')
+fig.show()
